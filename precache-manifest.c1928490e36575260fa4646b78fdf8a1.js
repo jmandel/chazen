14a5ab27833281dee95d7673c10e627141586372
Josh Mandel <jmandel@med.harvard.edu>
2019-10-21 22:09:37 -0500

    Updates

---
+++ precache-manifest.c1928490e36575260fa4646b78fdf8a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dd260f4d972c9f2dc597a8eab25582",
+    "url": "/chazen/index.html"
+  },
+  {
+    "revision": "9e3f331afdcbb56f76b6",
+    "url": "/chazen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5464e0f0ecad25ea0761",
+    "url": "/chazen/static/js/2.29808824.chunk.js"
+  },
+  {
+    "revision": "9e3f331afdcbb56f76b6",
+    "url": "/chazen/static/js/main.602963c7.chunk.js"
+  },
+  {
+    "revision": "7c4d7268d6894f6742ee",
+    "url": "/chazen/static/js/runtime-main.174cd17f.js"
+  }
+]);
