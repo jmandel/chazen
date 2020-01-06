e1da96a4d5aca4c1fb2d5ffcab1f694501131ca3
Josh Mandel <jmandel@med.harvard.edu>
2020-01-05 23:16:32 -0600

    Updates

---
+++ precache-manifest.7deb9588c8bb9ac1d27e360c0511e32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b4c207c0bbeae9423ebdc69e5c0dfa",
+    "url": "/chazen/index.html"
+  },
+  {
+    "revision": "6bbb2382c51e602fa0b2",
+    "url": "/chazen/static/css/main.faa7c01a.chunk.css"
+  },
+  {
+    "revision": "a18d71067e6e6a1174e1",
+    "url": "/chazen/static/js/2.b9e9962f.chunk.js"
+  },
+  {
+    "revision": "6bbb2382c51e602fa0b2",
+    "url": "/chazen/static/js/main.939fed40.chunk.js"
+  },
+  {
+    "revision": "7c4d7268d6894f6742ee",
+    "url": "/chazen/static/js/runtime-main.174cd17f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chazen/precache-manifest.7f729d6a293a1e9349124171060508b4.js"
+  "/chazen/precache-manifest.7deb9588c8bb9ac1d27e360c0511e32e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chazen/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
