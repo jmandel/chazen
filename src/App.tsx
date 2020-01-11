1cd99fc46c64e301551d9a66aaa51ade3a1d924d
Josh Mandel <jmandel@med.harvard.edu>
2020-01-11 16:08:09 -0600

    Gain node working on chrome but not mobile ios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef, useCallback, useState, Ref, RefObject } from "react";
 import { ActionTypes, Gallery } from "./ActionTypes";
 import "./App.css"
 
@@ -34,17 +34,19 @@ const defaultAppState: AppState = {
 };
 
 const appStateReducer = (state: AppState, action: ActionTypes): AppState => {
+  if (action.type !== "playbackStatus")
+    console.log(action)
   switch (action.type) {
     case "requestGallery":
-      return { ...state, gallery: action.gallery};
-     case "requestIteration":
+      return { ...state, gallery: action.gallery };
+    case "requestIteration":
       return { ...state, iteration: action.iteration || 0 };
     case "rollOverToIteration":
       return { ...state, iteration: action.iteration || 0 };
     case "playbackStatus":
-      return { ...state, offset: action.offset};
+      return { ...state, offset: action.offset };
     case "dismissModal":
-      return { ...state, modal: undefined};
+      return { ...state, modal: undefined };
     default:
       return state;
   }
@@ -53,79 +55,176 @@ const appStateReducer = (state: AppState, action: ActionTypes): AppState => {
 const PARALLEL_DOWNLOADS = 3
 const PROGRESS_INTERVAL_MS = 50
 
-  const dialTo = (steps: number, duration: number) => (elt: HTMLAudioElement, target: number) => {
-    const initial = elt.volume
-    const increment = (target - initial) / steps
-    for (let i=0; i < steps; i++) {
-      setTimeout(() => elt.volume = Math.min(1, Math.max(0, initial + (i+1) * increment)) , i* duration / steps)
-    }
-  }
-
-  const dialSoftly = dialTo(10, 250)
+const dialTo = (duration: number, target: number, gainNode: GainNode) => {
+  log(`Ramping ${duration}, ${target} ${gainNode}`)
+  gainNode.gain.setTargetAtTime(target, audioContext.currentTime, duration / 1000 / 3);
+  //gainNode.gain.value = target;
+}
 
 
+const BorderProgress: React.FC<{ fractionComplete: number }> = ({ fractionComplete }) => (
+  <>
+    <div key="0" style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: fractionComplete < .25 ? `${(fractionComplete - 0) * 4 * 100}%` : "100%",
+      borderTop: "1vmin solid black"
+    }}></div>
+    <div key="1" style={{
+      position: "absolute",
+      right: 0,
+      top: 0,
+      height: fractionComplete < .5 ? `${(fractionComplete - .25) * 4 * 100}%` : "100%",
+      borderRight: "1vmin solid black"
+    }}></div>
+    <div key="2" style={{
+      position: "absolute",
+      bottom: 0,
+      right: 0,
+      width: fractionComplete < .75 ? `${(fractionComplete - .50) * 4 * 100}%` : "100%",
+      borderBottom: "1vmin solid black"
+    }}></div>
+    <div key="3" style={{
+      position: "absolute",
+      left: 0,
+      bottom: 0,
+      height: fractionComplete < 1.0 ? `${(fractionComplete - .75) * 4 * 100}%` : "100%",
+      borderLeft: "1vmin solid black"
+    }}></div>
+  </>
+)
+
+const useGainedNode = () => {
+
+  const [gain, setGain] = useState<GainNode | null>(null);
+  const [node, setNode] = useState<HTMLAudioElement>();
+  const ref = useCallback((audioNode: HTMLAudioElement) => {
+    log(`GAining audio node ${audioNode}`);
+    const source = audioContext.createMediaElementSource(audioNode);
+    const gain = audioContext.createGain();
+    source.connect(gain);
+    gain.connect(audioContext.destination);
+    log(`connected audio node ${gain}`);
+    log(`with gain value  ${gain.gain.value}`);
+    setGain(gain)
+    setNode(audioNode)
+  }, [])
+  return [ref, gain, node] as const
+}
+
+const getAudioElement = (ref: any) => {
+  return ref[2] as (HTMLAudioElement | null)
+}
+
+const log = (s: string) => {
+  const l = document.getElementById("log")
+  l!.innerText = l!.innerText + "\n" + s;
+}
+
+window.onerror = function (message, file, line, col, error) {
+  log("Error occurred: " + (error as any).message);
+  return false;
+};
+
+window.addEventListener("error", function (e) {
+  log("Error occurred: " + e.error.message);
+  return false;
+})
+
+log("LOG")
+
+console.log("ACState", audioContext.state)
 
 const App: React.FC = () => {
   const [state, dispatch] = useReducer(appStateReducer, defaultAppState);
 
-  const audioElements: Record<Gallery, any> = {
-    [Gallery.study]: useRef<HTMLAudioElement>(null),
-    [Gallery.court]: useRef<HTMLAudioElement>(null),
-    [Gallery.niche]: useRef<HTMLAudioElement>(null),
-    [Gallery.bridge]: useRef<HTMLAudioElement>(null)
+  const audioElements: Record<Gallery, readonly [Ref<HTMLAudioElement>, GainNode | null, HTMLAudioElement | undefined]> = {
+    [Gallery.study]: useGainedNode(),
+    [Gallery.court]: useGainedNode(),
+    [Gallery.niche]: useGainedNode(),
+    [Gallery.bridge]: useGainedNode()
   };
 
   useEffect(() => {
-    let destroy = setInterval(()=> {
-      const offset = audioElements[state.gallery].current!.currentTime
-      dispatch({
-        type: "playbackStatus",
-        offset
-      })
+    log(`redo effect noAe ${audioElements[state.gallery][2]}`)
+    if (audioElements[state.gallery][2]) {
+      log(`t = ${audioElements[state.gallery][2]!.src}`)
+    }
 
-      const previousIteration = state.iteration
-      const currentIteration = Math.floor(offset/ITERATION_DURATION)
-      if (previousIteration !== currentIteration){
+    let destroy = setInterval(() => {
+      const currentAudio = getAudioElement(audioElements[state.gallery])
+      if (currentAudio) {
+
+        const offset = currentAudio.currentTime
         dispatch({
-          type: "rollOverToIteration",
-          iteration: currentIteration
+          type: "playbackStatus",
+          offset
         })
+
+        const previousIteration = state.iteration
+        const currentIteration = Math.floor(offset / ITERATION_DURATION)
+        if (previousIteration !== currentIteration) {
+          console.log("Rollover iteration")
+          dispatch({
+            type: "rollOverToIteration",
+            iteration: currentIteration
+          })
+        }
+
+      }
+      else {
+        console.log("Waiting for curfnet audio.")
       }
     }, PROGRESS_INTERVAL_MS)
 
     return () => clearInterval(destroy)
-  }, [state.gallery, state.iteration])
+  }, [state.gallery, state.iteration, audioElements[state.gallery][2]])
 
   const ITERATION_DURATION = 64.32 // 76;
   const LOOKBACK_ON_SWITCH = 0;
+  const VOLUME_RAMP_TIME = 500;
 
   const fractionComplete = (state.offset % ITERATION_DURATION) / ITERATION_DURATION
-  const iterationList: number[] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  const iterationList: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   // iteration_duration = 64.32s
   return (
     <div className="App">
-        {state.modal && <div className="modal">
-          <div className="modal-content">
-            <button
-              className="button start-button"
-              onClick={() => {
-                audioElements[state.gallery].current!.play();
-                dispatch({type: "dismissModal"})
-              }}
-            >Begin</button>
-          </div></div>}
+      {state.modal && <div className="modal">
+        <div className="modal-content">
+          <button
+            className="button start-button"
+            onClick={() => {
+              log(`AC status: ${audioContext.state}`)
+              const currentAudio = getAudioElement(audioElements[state.gallery])
+              if (currentAudio) {
+                currentAudio.play()
+                audioContext.resume().then(() => {
+                  log(`played then AC status: ${audioContext.state}`)
+                  dispatch({ type: "dismissModal" })
+                })
+              }
+            }}
+          >Begin</button>
+        </div></div>}
 
       <div className="wrapper">
-        <header className="header"> I am standing in a museum... {false && Math.round(state.offset*100)/100}
+        <header className="header"> I am standing in a museum... {false && Math.round(state.offset * 100) / 100}
           {GalleryNames.map(([description, gallery]) => (
-            <audio key={gallery} ref={audioElements[gallery]} loop={true} preload="auto" controls src={`${Gallery[gallery]}.mp3`} className="begin" />
+            <audio key={gallery}
+              ref={audioElements[gallery][0]}
+              loop={true}
+              preload="auto"
+              controls
+              src={`${Gallery[gallery]}.mp3`}
+              className="begin" />
           ))}
         </header>
         <div className="menu"></div>
         <div className="gutter"></div>
         {iterationList.map((i, c) => {
-          
+
           const src = `icon-${Gallery[state.gallery]}-${String(c).padStart(2, '0')}.svg`
           const gridArea = `iteration${Math.floor(c / 4)}${c % 4}`
           const className = `iteration 
@@ -134,77 +233,52 @@ const App: React.FC = () => {
               `;
 
           return (
-          <span 
-            key={c}
-            className={className}
-            style={{ gridArea }}>
-              {i === state.iteration && <>
-              <div key="0"  style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: fractionComplete  < .25 ? `${(fractionComplete - 0) * 4 * 100}%` : "100%",
-                borderTop: "1vmin solid black"
-              }}></div>
-              <div key="1"  style={{
-                position: "absolute",
-                right: 0,
-                top: 0,
-                height: fractionComplete  < .5 ? `${(fractionComplete - .25)*4 * 100}%` : "100%",
-                borderRight: "1vmin solid black"
-              }}></div>
-              <div key="2"  style={{
-                position: "absolute",
-                bottom: 0,
-                right: 0,
-                width: fractionComplete  < .75 ? `${(fractionComplete - .50)*4 * 100}%` : "100%",
-                borderBottom: "1vmin solid black"
-              }}></div>
-              <div key="3"  style={{
-                position: "absolute",
-                left: 0,
-                bottom: 0,
-                height: fractionComplete  < 1.0 ? `${(fractionComplete - .75)*4 * 100}%` : "100%",
-                borderLeft: "1vmin solid black"
-              }}></div>
- 
-              </>}
-             
-              <img src={src} 
+            <span key={c} className={className} style={{ gridArea }}>
+              {i === state.iteration && <BorderProgress fractionComplete={fractionComplete}></BorderProgress>}
+              <img src={src}
                 onClick={() => {
-                  GalleryNames.filter(([desc,g]) => g!== state.gallery).forEach(([desc,g]) => audioElements[g].current!.pause())
-                  audioElements[state.gallery].current!.play()
-                  audioElements[state.gallery].current!.currentTime = ((audioElements[state.gallery].current!.currentTime - LOOKBACK_ON_SWITCH) % ITERATION_DURATION) + c * ITERATION_DURATION
-                  dispatch({
-                    type: "requestIteration",
-                    iteration: i
-                  })
+                  const currentAudio = getAudioElement(audioElements[state.gallery]);
+                  if (currentAudio) {
+                    currentAudio.play()
+                    const targetTime = currentAudio.currentTime - LOOKBACK_ON_SWITCH
+                    currentAudio.currentTime = (targetTime % ITERATION_DURATION) + c * ITERATION_DURATION
+                    dispatch({
+                      type: "requestIteration",
+                      iteration: i
+                    })
+                  }
                 }
                 }>
-          </img>
-          </span>
-        )}
+              </img>
+            </span>
+          )
+        }
         )}
         <footer className="footer">
           <div className="gallery-selection">
             {GalleryNames.map(([description, gallery]) => (
               <button
-              key={gallery} 
-              onClick={() => {
-                dispatch({
-                  type: "requestGallery",
-                  gallery
-                })
-                const atTime = audioElements[state.gallery].current!.currentTime;
-                GalleryNames.forEach(([desc,g]) => 
-                {
-                  g !== gallery && dialSoftly(audioElements[g].current!, 0)
-                })
-                  audioElements[gallery].current!.currentTime = atTime
-                  audioElements[gallery].current!.play()
-                  dialSoftly(audioElements[gallery].current!, 1)
-              }}
-              className={`
+                key={gallery}
+                onClick={() => {
+                  const currentAudio = getAudioElement(audioElements[state.gallery]);
+                  if (currentAudio) {
+                    const atTime = currentAudio.currentTime;
+                    console.log("Requeste gal at", atTime)
+                    const nextAudio = getAudioElement(audioElements[gallery])!;
+                    nextAudio.currentTime = atTime
+                    nextAudio.play()
+                    GalleryNames.forEach(([desc, g]) => {
+                      g !== gallery && dialTo(VOLUME_RAMP_TIME, 0, audioElements[g][1]!)
+                    })
+                    dialTo(VOLUME_RAMP_TIME, 1, audioElements[gallery][1]!)
+
+                    dispatch({
+                      type: "requestGallery",
+                      gallery
+                    })
+                  }
+                }}
+                className={`
                 ${state.gallery === gallery ? "selected" : "default"}
                 ${Gallery[state.gallery]}
               `}>{description}</button>
